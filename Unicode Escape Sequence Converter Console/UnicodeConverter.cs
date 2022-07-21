5aafab1211aef0f644486766b9c4b30e0a60b07a
BeneX Drake <50136177+Benexdrake@users.noreply.github.com>
2022-07-21 22:41:13 +0200

    a Unicode Converter

---
+++ Unicode Escape Sequence Converter Console/UnicodeConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+using System.Text.RegularExpressions;
+
+
+namespace Unicode_Escape_Sequence_Converter_Console
+{
+    public class UnicodeConverter
+    {
+        private static readonly Regex Regex = new Regex(@"\\[uU]([0-9A-Fa-f]{4})");
+        public static async Task<string> UnescapeUnicode(string str)
+        {
+            return Regex.Replace(str,
+                match => ((char)int.Parse(match.Value.Substring(2),
+                    NumberStyles.HexNumber)).ToString());
+        }
+    }
+}
